8589b29e01d2e2f3a4ab3933839e2cfddbd93b6e
nelson valdivia <nperez88@gmail.com>
2016-02-25 18:54:25 -0800

    added iterative index views of Posts, set to homepage

--- config/routes.rb
+++ config/routes.rb
@@ -2,11 +2,11 @@ Rails.application.routes.draw do
 
   # get 'index/show'
   # get 'index/destroy'
-  root "users#index"
-  # resources :posts
-  get 'login', to: 'sessions#new'
-  get "logout", to: 'sessions#destroy'
-  resources :users
+  # root "users#index"
+  resources :posts
+  # get 'login', to: 'sessions#new'
+  # get "logout", to: 'sessions#destroy'
+  # resources :users
 
   resources :sessions, only: [:create]
 
