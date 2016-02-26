21a3f4c967d75989913182e9ff88733feca5f2de
nelson valdivia <nperez88@gmail.com>
2016-02-25 17:42:57 -0800

    deployed to heroku

--- config/routes.rb
+++ config/routes.rb
@@ -1,18 +1,20 @@
 Rails.application.routes.draw do
 
-  get 'index/show'
+  # get 'index/show'
+  # get 'index/destroy'
+  # root "users#index"
+  # resources :posts
+ resources :users
 
-  get 'index/destroy'
-
-  root "users#index"
-  resources :users
-  resources :posts
+  get "/posts" => "posts#index"
 
   #resources :users, only: [:new, :create]
 
   # Create a better looking URL for logging in
-  resources :sessions, only: [:new, :create, :destroy]
-  get '/login', to: 'sessions#new'
+
+
+  # resources :sessions, only: [:new, :create, :destroy]
+  # get '/login', to: 'sessions#new'
 
 
 end
